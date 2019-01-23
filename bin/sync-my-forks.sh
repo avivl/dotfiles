33eef9356079edba8b110213e0040afa2db7e3b5
avivl <aviv.laufer@gmail.com>
2019-01-23 08:52:17 +0200

    chnged forks path

--- bin/sync-my-forks.sh
+++ bin/sync-my-forks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for dir in /Users/aviv.laufer/Git/avivl/forks-doit/*
+for dir in /Users/aviv.laufer/Git/forks/*
 do
-cd $dir 
+cd $dir
 echo syncing $dir
-$ZPLUG_HOME/repos/caarlos0/zsh-git-sync/git-sync 
-cd .. 
+$ZPLUG_HOME/repos/caarlos0/zsh-git-sync/git-sync
+cd ..
 done
