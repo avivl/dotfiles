8dcea4c1949ef665b709e4813bd0e57b856de91c
Aviv Laufer <avivl@fb.com>
2020-04-20 10:17:12 +0300

    update fonts install

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,5 +3,21 @@ git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 # clean-up a bit
-cd ..
 rm -rf fonts
+cd ..
+install() {
+	curl -L -s -o "$1/SourceCodePro-Light.ttf" \
+		https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Light.ttf
+}
+
+if [ "$(uname -s)" = "Darwin" ]; then
+	if command -v brew >/dev/null 2>&1; then
+		brew tap homebrew/cask-fonts
+		brew cask install font-source-code-pro
+	else
+		install ~/Library/Fonts
+	fi
+else
+	mkdir -p ~/.fonts
+	install ~/.fonts
+fi
